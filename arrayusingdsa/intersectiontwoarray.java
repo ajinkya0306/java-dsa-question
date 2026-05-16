5f4ec2a359ac69877f503587c540396955b3172f
Ajinkya <ajibhosale1718@gmail.com>
2026-05-16 12:56:40 +0530

    Added intersection and kadanes algorithm problems

---
+++ arrayusingdsa/intersectiontwoarray.java
@@ -0,0 +1,28 @@
+package arrayusingdsa;
+
+import java.util.HashSet;
+
+public class intersectiontwoarray {
+	public static HashSet<Integer> intersection(int a[],int b[]){
+		HashSet<Integer>set=new HashSet();
+		HashSet<Integer>set2=new HashSet();
+
+		for(int x:a) {
+			set.add(x);
+		}
+		for(int y:b) {
+			if(set.contains(y)) {
+				set2.add(y);
+			}
+		}
+		
+		return set2;
+	}
+	public static void main (String args[]) {
+		int a[]= {1,2,3,4};
+		int b[]= {3,4,5,6,7,8};
+		HashSet<Integer>output=intersection(a,b);
+		System.out.println(output);
+	}
+
+}
