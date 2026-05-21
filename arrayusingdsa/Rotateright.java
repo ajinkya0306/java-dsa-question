2a1fd0522e87f2cb8f07360e1f859948e16cfc21
Ajinkya <ajibhosale1718@gmail.com>
2026-05-21 23:04:49 +0530

    add Rotateright code

---
+++ arrayusingdsa/Rotateright.java
@@ -0,0 +1,36 @@
+package arrayusingdsa;
+
+public class Rotateright {
+	public static void reverse(int[] arr,int left,int right) {
+		while(left<right) {
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+
+			left++;
+			right--;
+		}
+	}
+	public static void rotate(int[] arr,int k) {
+		int n=arr.length;
+		 k=k%n;
+		
+		 reverse(arr,0,n-1);
+		 reverse(arr,0,k-1);
+		 reverse(arr,k,n-1);
+		
+		
+	}
+	public static void main (String args[]) {
+		int arr[]= {1,2,3,4,5,6,7};
+		
+		int k=4;
+		rotate(arr,k);
+
+        System.out.print("Right Rotated Array: ");
+		for(int x:arr) {
+			System.out.print(x+"");
+		}
+	}
+
+}
